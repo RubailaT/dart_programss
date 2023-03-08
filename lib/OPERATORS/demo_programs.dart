a7209c0fccff92a60e1a76bcacb677d5ce4dc341
RubailaT <rubaila.t@gmail.com>
2023-03-08 12:21:34 +0530

    a file has been added.

--- lib/OPERATORS/demo_programs.dart
+++ lib/OPERATORS/demo_programs.dart
@@ -1,6 +1,6 @@
 void main()
 {
-  print("Example of Assignment Operator:");
+  print("Example of Arithmetic Operator:");
   var a=10;
   var b=20;
  /*var result=0;
@@ -40,4 +40,43 @@ results=c/d;
      var s1=20;
      var s2=3;
   print("s1%=s2 is ${s1%=s2}");
+
+  print("Example of Relational Operator:")  ;
+  var c=30;
+  var d=20;
+  var result=c>d;
+  print("c>d is $result")  ;
+  var res1=c<d;
+  print("c<d is $res1")  ;
+  var res2=c!=d;
+  print("c!=d is $res2")  ;
+  var res3=c==d;
+  print("c==d is $res3") ;
+  var res4=c<=d;
+print("c<=d is $res4") ;
+var res5=c>=d;
+print("c>=d is $res4")    ;
+var res6=c!=d;
+print("c!=d is "+res6.toString()) ;
+
+print(" ExampleS Of Logical Operator:");
+var a1=30;
+var b1=20;
+var res7=a1>b1&&b1>a1;
+print("result is:"+res7.toString());
+
+var res8=a1>b1||b1>a1;
+print("result is $res8") ;
+
+var res9=a1!=b1;
+print("result is " +res9.toString())  ;
+
+print("Examples of Type Test Operator: ") ;
+int a3=10;
+print(a3 is int)   ;
+print(a3 is! int )     ;
+
+
+
+
 }
