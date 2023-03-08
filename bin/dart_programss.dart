d278ba6231742fc021bddf4f867c74c5f8b09880
Muhammed Izin <muhammedizin@Muhammeds-MacBook-Air.local>
2023-03-08 11:24:33 +0530

    Initial commit

---
+++ bin/dart_programss.dart
@@ -0,0 +1,5 @@
+import 'package:dart_programss/dart_programss.dart' as dart_programss;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_programss.calculate()}!');
+}
---
+++ lib/OPERATORS/demo_programs.dart
@@ -0,0 +1,43 @@
+void main()
+{
+  print("Example of Assignment Operator:");
+  var a=10;
+  var b=20;
+ /*var result=0;
+  double c=100.5;
+  double d=2.2;
+  double results=0;
+  result=a+b;
+  print(" a+b=$result");
+  result=a-b;
+  print(" a-b=$result");
+  result=a*b;
+  print("a*b=$result");
+results=c/d;
+  print("c/d=$results");
+                             */
+  print("Addition = ${a+b}");
+  print("a-b=${a-b}");
+  print("a*b=${a*b}");
+  print("a/b=${a/b}");
+
+  print("Assignment Operators:");
+  var n1=20;
+  var n2=5;
+  print("n1+=n2 is ${n1+=n2}");
+     var m1=20;
+     var m2=5;
+     m1-=m2;
+  print("m1-=m2 is ${m1}");
+
+   var p1=20;
+   var p2=5;
+  print("p1*=p2 is${p1*=p2}");
+  double n=100.50;
+  double m=5.5;
+ 
+  print("n/=m is ${n/=m}")  ;
+     var s1=20;
+     var s2=3;
+  print("s1%=s2 is ${s1%=s2}");
+}
